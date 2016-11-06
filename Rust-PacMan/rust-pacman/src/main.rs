6d2e93b46bb76baefdc0e089b8debb3ed58dea25
Vamsi Jandhayala <Vjan@Vamsis-MBP.home>
2016-11-06 15:38:27 -0500

    Piston Window Test

---
+++ Rust-PacMan/rust-pacman/src/main.rs
@@ -0,0 +1,19 @@
+extern crate piston;
+extern crate graphics;
+extern crate piston_window;
+
+use piston_window::*;
+
+fn main() {
+	let mut window: PistonWindow = 
+		WindowSettings::new("Rust Pacman", [900, 800]).exit_on_esc(true).build().unwrap();
+
+		while let Some(e) = window.next() {
+			window.draw_2d(&e, |c,g| {
+				clear([1.0; 4], g);
+            	rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+			});
+		}
+}
